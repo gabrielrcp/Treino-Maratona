d30b40dad306ff69f6ba29c47d4e3114081f9b35
Gabriel <gabrielrcp@gmail.com>
2009-06-09 18:31:23 +0000

    Sighseeing tour -> AC!!!!

--- Seletiva2009/0507/1637.cpp
+++ Seletiva2009/0507/1637.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 #define MAX 210
+#define INF (1<<30)
 
 int n;
 
@@ -20,14 +21,16 @@ bool acha_caminho(int s, int t)
   for(int i = 0; i <= n+1; i++)
     ant[i] = -1;
 
+  ant[s] = s;
   queue<int> Q;
   Q.push(s);
   while(!Q.empty()){
     int i = Q.front();
     Q.pop();
 
-    for(int j = 0; j <= n; j++){
-      if(ant[j] == -1 && fluxo[i][j] < dupla[i][j]){
+    for(int j = 0; j <= n+1; j++){
+      if(i == j) continue;
+      if(ant[j] == -1 && 0 <= fluxo[i][j] && fluxo[i][j] < dupla[i][j]){
 	Q.push(j);
 	ant[j] = i;
       }
@@ -39,21 +42,23 @@ bool acha_caminho(int s, int t)
 
 void incrementa_fluxo(int s, int t)
 {
-  int m = 0;
+  int m = INF;
   int i, j;
 
   j = t; i = ant[t];
-  do{
-    m = max(m, dupla[i][j] - fluxo[i][j]);
+  while(j != s){
+    m = min(m, dupla[i][j] - fluxo[i][j]);
     j = i;
     i = ant[i];
-  }while(i != s);
+  }
 
   j = t; i = ant[t];
-  do{
+  while(j != s){
     fluxo[i][j] += m;
     fluxo[j][i] -= m;
-  }while(i != s);
+    j = i;
+    i = ant[i];
+  }
 }
 
 bool fluxo_maximo(int s, int t)
@@ -62,44 +67,49 @@ bool fluxo_maximo(int s, int t)
     for(int j = 0; j <= n+1; j++)
       fluxo[i][j] = 0;
 
-
+  
   while(1){
     if(!acha_caminho(s, t))
       break;
     incrementa_fluxo(s, t);
   }
-
+  
   for(int j = 1; j <= n; j++){
     if(fluxo[s][j] < dupla[s][j])
       return false;
     if(fluxo[j][t] < dupla[j][t])
       return false;
   }
+  
   return true;
 }
 
 bool alcanca1[MAX], alcanca2[MAX];
 
+int lbl[MAX], low[MAX];
+int vis[MAX];
+int conta;
 
 
-bool conexos()
+
+void dfs(int i)
 {
-  int a = -1, b = -1;
-  for(int i = 1; i <= n; i++){
-    for(int j = 1; j <= n; j++){
-      if(unica[i][j] || fluxo[i][j])
-	a = i;
+  vis[i] = true;
+  lbl[i] = conta++;
+  low[i] = lbl[i];
+  for(int j = 1; j <= n; j++){
+    if(i == j)
+      continue;
+    if(unica[i][j] == 0 && fluxo[i][j] == dupla[i][j])
+      continue;
+
+    if(!vis[j]){
+      dfs(j);
+      low[i] = min(low[i], low[j]);
     }
+    else
+      low[i] = min(low[i], lbl[j]);
   }
-
-  for(int i = 1; i <= n; i++)
-    for(int j = i+1; j <= n; j++)
-      if(abs(fluxo[i][j]) < dupla[i][j])
-	b = i;
-
-
-
-
 }
 
 
@@ -111,11 +121,36 @@ bool vai()
     dupla[i][n+1] = -min(dupla[0][i], 0);
     dupla[0][i] = max(dupla[0][i], 0);    
   }
-
+  
   if(!fluxo_maximo(0, n+1))
     return false;
 
+  for(int i = 1; i <= n; i++){
+    int s = 0;
+    for(int j = 1; j <= n; j++)
+      s += dupla[i][j] - abs(fluxo[i][j]);
+    if(s % 2 == 1)
+      return false;
+  }
+    
+  for(int i = 1; i <= n; i++)
+    vis[i] = false;
 
+  conta = 0;
+  dfs(1);
+
+  /*
+  for(int i = 1; i <= n; i++)
+    printf("%d ", low[i]);
+  printf("\n");
+  */
+
+
+  for(int i = 1; i <= n; i++)
+    if((!vis[i]) || (low[i] > 0 && low[i] == lbl[i]))
+      return false;
+  
+  return true;
 }
 
 int main()
@@ -143,6 +178,7 @@ int main()
       printf("possible\n");
     else
       printf("impossible\n");
+    
   }
   return 0;
 }
