30e2b35336d881097b3e391f1e5cab6bc5ae7bfa
Gabriel <gabrielrcp@gmail.com>
2009-06-10 22:29:24 +0000

    Knights of the round table

--- Seletiva2009/0319/2942.cpp
+++ Seletiva2009/0319/2942.cpp
@@ -2,6 +2,8 @@
 #include <map>
 #include <cstdlib>
 #include <algorithm>
+#include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -25,13 +27,13 @@ void dfs(int i)
   ordem[i] = conta++;
   low[i] = ordem[i];
   for(int k = 0; k < num[i]; k++){
-    int j = G[i][j];
+    int j = G[i][k];
     if(ordem[j] == -1){
       f++;
       pai[j] = i;
       dfs(j);
       low[i] = min(low[i], low[j]);
-      if(low[j] >= ordem[i] && pai[i] != i)
+      if(pai[i] != i && low[j] >= ordem[i])
 	articula[i] = true;
     }
     else if(pai[i] != j)
@@ -46,7 +48,7 @@ void achaarticula()
 {
   conta = 1;
   for(int i = 0; i < n; i++){
-    ordem[i] = low[i] = pai[MAX] = -1;
+    ordem[i] = low[i] = pai[i] = -1;
     articula[i] = false;
   }
   
@@ -59,6 +61,66 @@ void achaarticula()
 }
 
 
+int M[MAX][MAX];
+char cor[MAX];
+
+bool bicolore(int i, int v, char c)
+{
+  cor[i] = c;
+  for(int k = 0; k < num[i]; k++){
+    int j = G[i][k];
+    if(v != -1 && M[i][j] != v)
+      continue;
+    if(cor[j] == cor[i])
+      return false;
+    if(cor[j] + cor[i] == 1)
+      continue;
+
+    if(!bicolore(j, M[j][i], 1 - c))
+      return false;
+  }
+  return true;
+}
+
+void mapear(int art, int i, int v)
+{
+  M[art][i] = v;
+  
+  for(int k = 0; k < num[i]; k++){
+    int j = G[i][k];
+    if(j == art || M[art][j] != -1)
+      continue;
+    mapear(art, j, v);
+  }
+}
+
+
+int resolve()
+{
+  for(int i = 0; i < n; i++)
+    for(int j = 0; j < n; j++)
+      M[i][j] = -1;
+
+  for(int i = 0; i < n; i++)
+    if(articula[i]){
+      int s = 0;
+      for(int k = 0; k < num[i]; k++){
+	int j = G[i][k];
+	if(M[i][j] == -1)
+	  mapear(i, j, s++);
+      }
+    }
+
+  int r = 0;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++)
+      cor[j] = -1;
+    if(bicolore(i, -1, 0))
+      r++;
+  }
+  return r;
+} 
+
 int main()
 {
   int m;
@@ -67,7 +129,7 @@ int main()
     if(n == m && m == 0) break;
     for(int i = 0; i < n; i++){
       for(int j = 0; j < n; j++)
-	mat[i][j] = true;
+	mat[i][j] = (i != j);
     }
     
     while(m--){
@@ -85,7 +147,10 @@ int main()
 	  num[i]++;
 	}
     }
+
     achaarticula();
+    printf("%d\n", resolve());
+
   }
   return 0;
 }
