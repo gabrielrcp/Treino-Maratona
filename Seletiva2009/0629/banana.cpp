5cdb13e467a4108f78737309d8eaf53b5f2f6e13
Gabriel <gabrielrcp@gmail.com>
2009-07-01 03:14:27 +0000

    Banana e Area

--- Seletiva2009/0629/banana.cpp
+++ Seletiva2009/0629/banana.cpp
@@ -3,17 +3,96 @@
 
 using namespace std;
 
+#define MAX 100010
+#define INF (1<<29)
+
 struct node{
   int orig, dest;
   int at, melhor;
 };
 
 vector< vector<node> > G;
+vector<int> pai;
 int n;
+
+int minimo[MAX];
+int atual[MAX];
+
+
+void monta(int i)
+{
+  minimo[i] = atual[i] = 0;
+  for(int k = 0; k < G[i].size(); k++){
+    int j = G[i][k].dest;
+    if(pai[i] != j && pai[j] == -1){
+      pai[j] = i;
+      monta(j);
+      minimo[i] = max(minimo[j]+G[i][k].melhor, minimo[i]);
+      atual[i] = max(atual[j]+G[i][k].at, atual[i]);
+    }
+  }
+}
+
+int gasta(int i, int target)
+{
+  if(minimo[i] > target)
+    return INF;
+  if(atual[i] <= target)
+    return 0;
+
+  int r = 0;
+  for(int k = 0; k < G[i].size(); k++){
+    int j = G[i][k].dest;
+    if(pai[i] == j) continue;
+
+    int x = G[i][k].at + atual[j];
+    if(x <= target)
+      continue;
+    int t = G[i][k].at - G[i][k].melhor;
+    if(atual[j] <= target)
+      r += x - target;
+    else
+      r += t + gasta(j, target-G[i][k].melhor);
+  }
+  return r;
+}
 
 
 int main()
 {
+  int k;
   scanf(" %d %d", &n, &k);
+  G = vector< vector<node> > (n);
+  pai = vector<int> (n, -1);
+
+  for(int k = 1; k < n; k++){
+    int i, j, o, a;
+    scanf(" %d %d %d %d", &i, &j, &a, &o);
+    i--; j--;
+
+    node novo;
+    novo.at = a;
+    novo.melhor = o;
+    
+    novo.orig = i;
+    novo.dest = j;
+    G[i].push_back(novo);
+
+    novo.orig = j;
+    novo.dest = i;
+    G[j].push_back(novo);
+  }
+
+  monta(0);
+
+  int e = 0, d = INF;
+  while(e < d){
+    int m = (e+d)/2;
+    if(gasta(0, m) <= k)
+      d = m;
+    else
+      e = m + 1;
+  }
+  printf("%d\n", e);
   return 0;
 }
