934b3ffd84390ce7fd7d6369dfcade6243212046
Gabriel <gabrielrcp@gmail.com>
2009-07-05 16:44:11 +0000

    Tentando melhorar o tempo do kokos... (sem sucesso)

--- Seletiva2009/0629/kokos.c
+++ Seletiva2009/0629/kokos.c
@@ -1,37 +1,41 @@
-#include <cstdio>
+#include <stdio.h>
+#include <ctype.h>
 
-struct node{
-  struct node *f[26];
-};
+typedef struct _node{
+  struct _node *f[26];
+} node;
 
 node _nos[2000100];
 int conta_nos;
 
 node *novo_no()
 {
+  int i;
   node *novo = &_nos[conta_nos];
   conta_nos++;
-  for(int i = 0; i < 26; i++)
+  for(i = 0; i < 26; i++)
     novo->f[i] = NULL;
   return novo;
 }
 
-void insere(node *arv, char *s, int inc)
+void insere(node *arv, char *s)
 {
+  int i;
   if(*s == '\0')
     return;
-  int i = *s - 'A';
+  i = *s - 'A';
   if(arv->f[i] == NULL)
     arv->f[i] = novo_no();
-  insere(arv->f[i], s+inc, inc);
+  insere(arv->f[i], s+1);
 }
 
 int conta(node *arv)
 {
+  int r, i;
   if(arv == NULL)
     return 0;
-  int r = 1;
-  for(int i = 0; i < 26; i++)
+  r = 1;
+  for(i = 0; i < 26; i++)
     r += conta(arv->f[i]);
   return r;
 }
@@ -42,21 +46,28 @@ int main()
 {
   int n, k;
   node *arv1, *arv2;
+  int i, j;
+  char c;
   scanf(" %d %d", &n, &k);
 
   conta_nos = 0;
   arv1 = novo_no();
   arv2 = novo_no();
 
-  for(int i = 0; i < n; i++){
-    scanf(" %s", buf);
-    char c = buf[k];
-    buf[k] = '\0';
-    insere(arv1, buf, 1);
+  buf[k] = '\0';
+  for(i = 0; i < n; i++){
+    while(isspace(c = getchar()));
+    buf[0] = c;
 
-    buf[k] = c;
-    buf[k-1] = '\0';
-    insere(arv2, buf+2*k-1, -1);
+    for(j = 1; j < k; j++)
+      buf[j] = getchar();
+    insere(arv1, buf);
+
+
+    for(j = k-1; j >= 0; j--)
+      buf[j] = getchar();
+    insere(arv2, buf);
+
   }
   printf("%d\n", conta(arv1) + conta(arv2) - 2);
 
