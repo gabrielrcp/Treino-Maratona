05868fa0d7ee9a3719286ed78cd92b08633244b4
Gabriel <gabrielrcp@gmail.com>
2009-07-19 21:39:03 +0000

    Huffman -> TLE

--- Seletiva2009/0430/1261.cpp
+++ Seletiva2009/0430/1261.cpp
@@ -1,47 +1,157 @@
 #include <cstdio>
 #include <cstring>
 
-int z, n;
-char codifica[300];
+using namespace std;
 
-int posicoes[21];
+#define MAX 210
 
-bool valida()
+struct node{
+  int numf;
+  struct node *f[10];
+};
+
+node _nos[MAX];
+node *ll[MAX];
+int topo;
+
+int n, z;
+char codifica[MAX];
+
+node *tree;
+int tam, lim;
+
+node *novo()
 {
-  return false;
+  node *nv = ll[topo--];
+  for(int i = 0; i < n; i++)
+    nv->f[i] = NULL;
+  nv->numf = 0;
+  return nv;
 }
 
-bool tenta(int pat, int ini)
+void devolve(node *no)
 {
-  if(pat+1 == z)
-    return valida();
+  ll[++topo] = no;
+}
 
-  for(int i = ini; codifica[i] != '\0'; i++){
-    posicoes[pat] = i;
-    if(tenta(pat+1, i))
-      return true;
+void inicializa()
+{
+  for(int i = 0; i < MAX; i++)
+    ll[i] = _nos+i;
+  topo = MAX-1;
+  tree = novo();
+  tam = strlen(codifica);
+  lim = 12;
+}
+
+bool verifica_completa(node *arv)
+{
+  if(arv->numf == 0)
+    return true;
+  if(arv->numf < n)
+    return false;
+  for(int i = 0; i < n; i++)
+    if(!verifica_completa(arv->f[i]))
+      return false;
+  return true;
+}
+
+
+int marcas[MAX];
+
+bool vai(int ini, int falta)
+{
+  if(falta == 0 && ini == tam){
+    if(!verifica_completa(tree))
+      return false;
+    for(int k = 0; k < z; k++){
+      printf("%d->", k);
+      int j = (k == 0 ? 0 : marcas[k-1]+1);
+      while(j <= marcas[k]){
+	putchar(codifica[j]);
+	j++;
+      }
+      putchar('\n');
+    }
+    return true;
   }
+  if(falta == 0 || ini == tam)
+    return false;
+
+  node *arv = tree;
+  
+  int i = ini;
+  while(1){
+    char c = codifica[i];
+    if(c == '\0')
+      return false;
+    if(arv->numf == 0 && arv != tree)
+      return false;
+    if(arv->f[c-'0'] == NULL)
+      break;
+    arv = arv->f[c-'0'];
+    i++;
+  }
+
+  if(i - ini > lim)
+    return false;
+
+  if(falta == 1){
+    if(i + 1 < tam)
+      return false;
+    arv->numf++;
+    arv->f[codifica[i]-'0'] = novo();
+    marcas[z-1] = tam - 1;
+
+    if(vai(tam, 0))
+      return true;
+
+    devolve(arv->f[codifica[i]-'0']);
+    arv->f[codifica[i]-'0'] = NULL;
+    arv->numf--;
+    return false;
+  }
+
+  int j = i;
+  node *atual = arv, *nv;
+  while(j + falta < tam && j - ini <= lim){
+    int k = codifica[j] - '0';
+      
+    nv = novo();
+    atual->f[k] = nv;
+    atual->numf++;
+    marcas[z-falta] = j;
+
+    if(falta + atual->numf < n)
+      break;
+    if(vai(j+1, falta-1))
+      return true;
+
+    atual = nv;
+    j++;
+  }
+
+  atual = arv->f[codifica[i]-'0'];
+  arv->f[codifica[i]-'0'] = NULL;
+  arv->numf--;
+  i++;
+  while(1){
+    if(atual == NULL)
+      break;
+    devolve(atual);
+    atual = atual->f[codifica[i]-'0'];
+    i++;
+  }
+
   return false;
+
 }
 
 
 void resolve()
 {
-  tenta(0, 0);
-
-  printf("0->");
-  for(int j = 0; j < posicoes[0]; j++)
-    printf("%c", codifica[j]);
-  printf("\n");
-
-
-  posicoes[z-1] = strlen(codifica);
-  for(int i = 1; i < z; i++){
-    printf("%d->", i);
-    for(int j = posicoes[i-1]; j < posicoes[i]; j++)
-      printf("%c", codifica[j]);
-    printf("\n");
-  }
+  inicializa();
+  vai(0, z);
 }
 
 
