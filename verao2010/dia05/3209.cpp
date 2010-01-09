81dd537e66df32d3d879e7b95529ece1b19b4029
Gabriel <gabrielrcp@gmail.com>
2010-01-09 04:01:43 +0000

    Pithagoras (apertado!)

--- verao2010/dia05/3209.cpp
+++ verao2010/dia05/3209.cpp
@@ -3,7 +3,7 @@
 
 typedef long long ll;
 
-#define MAX ((1<<25)+1)
+#define MAX ((1<<23)+1)
 
 char nep[MAX];
 int npr;
@@ -34,14 +34,14 @@ bool vai(ll n)
 {
   if(n < 0) return false;
   if(n%4 == 3) return false;
-  for(int i = 0; i < npr; i++){
+  for(int i = 0; i < npr && n > 1 && (ll)primos[i]*primos[i] <= n; i++){
     int p = primos[i];
     int c = 0;
     while(n > 1 && (n % p == 0)){
       n /= p;
       c++;
     }
-    if((p % 4 == 3) && (c % 2 == 1))
+    if((p % 4 == 3) && (c & 1))
       return false;
   }
   return (n % 4 != 3);
