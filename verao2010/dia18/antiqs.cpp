5eaf61e10944b7d0571f99aa2fce9b5dc2db3dbb
Gabriel <gabrielrcp@gmail.com>
2010-01-24 04:14:55 +0000

    Antiquicksort

--- verao2010/dia18/antiqs.cpp
+++ verao2010/dia18/antiqs.cpp
@@ -10,59 +10,15 @@
 #include <iostream>
 #include <sstream>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
-#define MAX 70010
-
 int n;
-int resp[MAX];
-int arv[5*MAX];
 
-void monta(int x, int e, int d)
+void imprime(vector <int> &v)
 {
-  int m = (e+d)/2;
-  arv[x] = d - e + 1;
-  if(e < d){
-    monta(2*x+1, e, m);
-    monta(2*x+2, m+1, d);
-  }
-}
-
-int busca(int val)
-{
-  int x = 0, e = 0, d = n-1;
-  while(e < d){
-    int f = 2*x+1;
-    int m = (e+d)/2;
-    if(arv[f] >= val){
-      d = m;
-    } else{
-      val -= arv[f];
-      f++;
-      e = m + 1;
-    }
-    x = f;
-  }
-  return e;
-}
-
-void update(int pos)
-{
-  int x = 0, e = 0, d = n-1;
-  while(e < d){
-    arv[x]--;
-    int m = (e+d)/2;
-    int f = 2*x+1;
-    if(pos <= m)
-      d = m;
-    else{
-      e = m + 1;
-      f++;
-    }
-    x = f;
-  }
-  arv[x]--;
+  //printf("%d\n", QSort(v, 0, n-1));
 }
 
 int main()
@@ -74,18 +30,35 @@ int main()
   
   scanf(" %d", &n);
 
-  monta(0, 0, n-1);
+  vector<int> v (n, -1);
+
+
+  if(n == 1){
+    v[0] = 1;
+  } else if(n == 2){
+    v[0] = 1; v[1] = 2;
+  }else{
+    stack<int> P;
+    P.push(n);
+    while(!P.empty()){
+      int i = P.top(); P.pop();
+      if(i == 3){
+	v[0] = 1; v[1] = 3; v[2] = 2;
+      } else if(v[i-1] == -1){
+	v[i-1] = i;
+	P.push(i);
+	P.push(i-1);
+	continue;
+      } else{		       
+	int m = (i-1)/2;
+	swap(v[i-1], v[m]);
+      }
+    }
+  }
 
   for(int i = 0; i < n; i++){
-    int m = ((i+n-1) / 2) - i;
-    int r = busca(m+1);
-    update(r);
-    resp[r] = i+1;
-  }
-  
-  for(int i = 0; i < n; i++){
     if(i != 0) printf(" ");
-    printf("%d", resp[i]);
+    printf("%d", v[i]);
   }
   printf("\n");
   
